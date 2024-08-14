6a76f2bba55a77f9cf9ac94eaec263906f14b812
xinwen <xinwen618@gmail.com>
2024-08-15 00:49:11 +0800

    Implementation of rope method

--- rope.py
+++ rope.py
@@ -5,6 +5,7 @@ class LlamaRotaryEmbedding(nn.Module):
     def __init__(self, dim, max_position_embedding=2048, base=10000, device = None):
         super().__init__()
         inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float().to(device) / dim))
+        # self.inv_freq 是预定义的逆频率张量，维度为 [head_size/2]，用于生成位置编码。
         self.register_buffer("inv_freq", inv_freq)
 
         # build here to make 'torch.jit.trace' work
@@ -21,14 +22,23 @@ class LlamaRotaryEmbedding(nn.Module):
 
     def forward(self, x, seq_len=None):
         # x: [bs, num_attention_heads, seq_len, head_size] 
+        # 如果新的 seq_len 大于之前的最大长度，则更新 self.max_seq_len_cached，以便生成新的缓存。
         if seq_len > self.max_seq_len_cached:
             self.max_seq_len_cached = seq_len
+            # t 是一个形状为 [self.max_seq_len_cached] 的张量，包含从 0 到 self.max_seq_len_cached - 1 的整数，表示每个位置的索引。
             t = torch.arange(self.max_seq_len_cached, device=x.device, dtype=self.inv_freq.dtype)
+            # torch.einsum("i,j->ij", t, self.inv_freq) 计算了位置索引和频率的外积。生成的 freqs 张量形状为 [seq_len, head_size/2]，表示位置编码频率。
+            # 通过 torch.einsum 计算 freqs，它表示每个位置和每个维度的旋转角度。将 freqs 拼接成两倍长度，以便与嵌入向量中的所有维度进行匹配。
             freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+            # 将 freqs 张量沿最后一个维度（-1）进行拼接，生成形状为 [seq_len, head_size] 的张量 emb，这用于后续计算正弦和余弦。
             emb = torch.cat((freqs, freqs), dim = -1).to(x.deivce)
+            # 缓存张量的形状为 [1, 1, seq_len, head_size]，并且将其数据类型转换为与输入张量 x 相同的类型 x.dtype。
+            # persistent=False 表示这些缓存张量不会被保存到模型的状态字典中（不会随模型保存）。
             self.register_buffer("cos_cached", emb.cos()[None, None, :, :].to(x.dtype), persistent=False)
             self.register_buffer("sin_cached", emb.sin()[None, None, :, :].to(x.dtype), persistent=False)
 
+        # 返回两个张量，分别是缓存的正弦和余弦值的切片。根据当前输入的 seq_len，提取对应长度的部分。
+        # 这些张量的形状为 [1, 1, seq_len, head_size]，并被转换为与输入张量 x 相同的数据类型。
         return (
             self.cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
             self.sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype)
